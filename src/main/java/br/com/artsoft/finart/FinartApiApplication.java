fd4e55eb8222d9bbdeabe72ad07278f33333c756
DESKTOP-IB3PDCC\arthu <arthur.2co@gmail.com>
2022-03-16 19:21:02 -0300

    config inicial

---
+++ src/main/java/br/com/artsoft/finart/FinartApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.artsoft.finart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinartApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinartApiApplication.class, args);
+	}
+
+}
